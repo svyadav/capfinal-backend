3f6f7db44ebb656abb4f6c6e6717b5c5ebe2699f
sachin <ysachin511@gmail.com>
2022-11-30 21:42:37 +0530

    chnage url

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-const CLIENT_URL = "animated-pika-2330c5.netlify.app";
+const CLIENT_URL = "http://localhost:3000";
 
 router.get("/login/success", (req, res) => {
   if (req.user) {
